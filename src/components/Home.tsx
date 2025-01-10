83545f8639480fca7e7d10564c8ac6a915b283d0
benie254 <davinci.monalissa@gmail.com>
2025-01-10 15:12:37 +0300

    change (home): update home content

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,10 @@
 export default function HomeComponent() {
   return (
     <div>
-      <h1>React Starter Kit</h1>
+      <h1>Accountable</h1>
       <p>
-        Use this template repository as a starter kit <br /> for building
-        projects in{" "}
-        <code>
-          <b>React</b>
-        </code>{" "}
-        <br />
-        with{" "}
-        <code>
-          <b>TypeScript</b>
-        </code>{" "}
-        and{" "}
-        <code>
-          <b>SCSS</b>
-        </code>
+        An "accountability buddy" app to help keep track of goal and activity
+        progresses.
       </p>
     </div>
   );
